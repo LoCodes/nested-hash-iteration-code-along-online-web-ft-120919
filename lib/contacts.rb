bf5f63e3478add9ddaad7d461143a173dbe0d7d8
Loren Cipriano <loren25cipriano@gmail.com>
2019-12-18 23:07:25 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,5 +16,8 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
+    if person == "Fredy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
 
 end
